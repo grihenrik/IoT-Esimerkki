c3fa9fe845b0a9da982b3452d5325c2d7030b137
Henrik Gripenberg <Henrik.Gripenberg@student.lamk.fi>
2019-04-08 15:45:55 +0300

    Added inputs from gyroscope, acceleration meter and magnetic field meter

--- IoT-Esimerkki/MainPage.xaml.cs
+++ IoT-Esimerkki/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Emmellsoft.IoT.Rpi.SenseHat;
+using RichardsTech.Sensors;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -24,7 +28,55 @@ namespace IoT_Esimerkki
     {
         public MainPage()
         {
+            
             this.InitializeComponent();
+            tstTxt.Text = "IoT-esimerkki";
+            Debug.WriteLine("Iot-esimerkki");
+
+
+        }
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await CollectSensorData(new Progress<Measurement>(m =>
+            {
+                tstTxt.Text = $"{m.Temperature:F2} degrees, {m.Humidity:F2} RH%, {m.Pressure:F2} mbar\n ACC: {m.Acceleration.ToString()} GYRO: {m.Gyroscope.ToString()} MagneticField: {m.MagneticField.ToString()} ";
+                Debug.WriteLine(tstTxt.Text);
+            }));
+        }
+
+        private async Task CollectSensorData(IProgress<Measurement> progress)
+        {
+            using (ISenseHat senseHat = await SenseHatFactory.GetSenseHat())
+            {
+                for(; ; )
+                {
+                    try
+                    {
+                        senseHat.Sensors.HumiditySensor.Update();
+                        senseHat.Sensors.PressureSensor.Update();
+                        senseHat.Sensors.ImuSensor.Update();
+
+                        var measurement = new Measurement()
+                        {
+                            Temperature = senseHat.Sensors.Temperature ?? 0,
+                            Humidity = senseHat.Sensors.Humidity ?? 0,
+                            Pressure = senseHat.Sensors.Pressure ?? 0,
+                            MagneticField =(Vector3) senseHat.Sensors.MagneticField,
+                            Gyroscope = (Vector3) senseHat.Sensors.Gyro,
+                            Acceleration = (Vector3) senseHat.Sensors.Acceleration
+                        };
+                        progress.Report(measurement);
+
+                        await AzureIoTHub.SendDeviceToCloudMessageAsync(measurement);
+
+                    } catch (Exception e)
+                    {
+                        Debug.WriteLine("Exception: " + e.Message);
+                    }
+                    await Task.Delay(1000);
+                }
+            }
         }
     }
 }
---
+++ IoT-Esimerkki/Measurement.cs
@@ -0,0 +1,21 @@
+﻿using RichardsTech.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT_Esimerkki
+{
+    class Measurement
+    {
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Pressure { get; set; }
+        public Vector3 Acceleration { get; set; }
+        public Vector3 Gyroscope { get; set; }
+        public Vector3 MagneticField { get; set; }
+        public string DeviceId { get; set; }
+
+    }
+}
